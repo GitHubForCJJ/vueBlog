bbb141f4b067dff45aad4988b61240345d9e766e
chenjianjun <chenjianjun@567na.com>
2019-11-21 18:58:19 +0800

    详情页

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,33 @@
 <template>
   <div id="app">
     <div class="blogapp">
-      <div class="aside">aside</div>
+      <div class="aside">
+        <div class="top">
+
+        </div>
+        <div class="bottom">
+          <div class="item"> <img src="./assets/icons/login1.png"
+                 alt="登录"></div>
+          <div class="item"> <img src="./assets/icons/sun.png"
+                 alt="白天"></div>
+          <div class="item"> <img src="./assets/icons/moon.png"
+                 alt="黑夜"></div>
+
+        </div>
+      </div>
       <div class="mainapp">
         <div class="main">
           <router-view></router-view>
           <!-- 备案信息 -->
-          <footer>©2015 /蜀ICP备18005286号-1</footer>
+          <footer>©2018 /蜀ICP备18005286号-1</footer>
         </div>
       </div>
-
-      <!-- <el-container class="elcon">
-        <el-aside class="aside"
-                  width='45px'>Aside
-        </el-aside>
-        <el-main>
-          <div class="main">
-            <router-view></router-view>
-          </div>
-
-        </el-main>
-      </el-container> -->
     </div>
 
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
--- src/views/blog/index.vue
+++ src/views/blog/index.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="container">
+    <!-- 标题 -->
+    <h1 class="blogTitle">" 服务端渲染(Universal/Isomorphic)之React"</h1>
+    <!-- 阅读信息 -->
+    <div class="viewInfo">
+      <div>阅读 <span>1823</span></div>
+      <div> . 评论 <span>101</span></div>
+      <div> . 喜欢 <span>22</span></div>
+    </div>
+
+    <!-- 内容 -->
+
     <!-- 评论 -->
     <div class="articleLike">
       <div class="likeTitle">喜欢</div>
@@ -19,15 +30,58 @@
 <style lang="less" scoped>
 .container {
   width: 100%;
-  padding-top: 30px;
+
+  h1 {
+    font-size: 36px;
+  }
+  h2 {
+    font-size: 30px;
+  }
+  h3 {
+    font-size: 24px;
+  }
+  h4 {
+    font-size: 18px;
+  }
+  h5 {
+    font-size: 14px;
+  }
+  h6 {
+    font-size: 12px;
+  }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: inherit;
+    font-weight: 500;
+    line-height: 1.1;
+    color: inherit;
+  }
+  p {
+    margin: 0 0 10px;
+  }
+
+  .blogTitle {
+    word-break: break-all;
+    font-size: 2.2em;
+    font-weight: bold;
+    line-height: 1.5;
+    color: #555;
+  }
+
+  .viewInfo {
+    font-size: 13.4px;
+    display: flex;
+  }
 
   pre {
     background-color: #fae5e2;
     color: #d96f5d;
-
     word-wrap: normal;
     word-break: normal;
-
     margin-bottom: 20px;
     border: 1px solid rgba(0, 0, 0, 0.15);
   }
@@ -81,8 +135,9 @@
     align-items: center;
     display: flex;
     justify-content: space-between;
-    font-size: 16px;
+
     color: #555;
+    font-size: 14px;
 
     .prev {
       //width: 45%;
